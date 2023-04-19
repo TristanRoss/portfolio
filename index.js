61b734fbb7afd1aab7189ac95114a0531b3a09f7
TristanRoss <tristanross1998@gmail.com>
2023-04-19 01:56:26 -0500

    Update portfolio site

---
+++ index.js
@@ -0,0 +1,16 @@
+new fullpage('#fullpage', {
+  //options here
+  licenseKey: 'gplv3-license',
+  navigation: true,
+  navigationPosition: 'right',
+  autoScrolling: true,
+  scrollHorizontally: true,
+  controlArrowsHTML: [
+    '<div class="fp-arrow"></div>',
+    '<div class="fp-arrow"></div>',
+  ],
+  showActiveTooltip: false,
+  slidesNavigation: true,
+  slidesNavPosition: 'bottom',
+  credits: { enabled: false },
+});
